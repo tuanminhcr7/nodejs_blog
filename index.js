e0096c885a512b7a732251c064198a4c05c373ea
tuanminhcr7 <69630374+tuanminhcr7@users.noreply.github.com>
2021-05-21 10:37:01 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// route
+app.get('/homepage', (req, res) => {
+    var a = 1;
+    var b = 2;
+    var c = a + b;
+  res.send('Hello World!')
+})
+
+// 127.0.0.1 - localhost
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
